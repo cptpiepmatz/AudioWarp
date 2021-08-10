593bc6b3f3b31bf965598ef347ee6ade19e9edd5
Tim Hesse <tim.hesse@uni-oldenburg.de>
2021-08-10 21:49:45 +0200

    Replaced icon stuff

--- scripts/icon.js
+++ scripts/icon.js
@@ -1,6 +1,13 @@
 const rcedit = require("rcedit");
 const path = require("path");
+const os = require("os");
+const fs = require("fs/promises");
 
-rcedit(path.join(__dirname, "../dist/audio-warp.exe"), {
+const prebuilt = path
+  .join(os.homedir(), "./.pkg-cache/v3.2/fetched-v16.4.1-win-x64");
+
+rcedit(prebuilt, {
   icon: path.join(__dirname, "../icon/icon.ico")
-}).catch(console.error);
+})
+  .then(() => fs.rename(prebuilt, prebuilt.replace("fetched-", "built-")))
+  .catch(console.error);
