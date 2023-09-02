eda213a238caf94eaa69fbdcb41a535a8e52a51a
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-09-02 11:58:46 +0200

    applied more setup to the main fn

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use std::borrow::Cow;
 use std::env::VarError;
 use std::io::Stdout;
+use std::ops::Deref;
 use std::sync::Arc;
 use std::time::Duration;
 use std::{env, io};
@@ -17,7 +18,7 @@ use crossterm::{event, execute};
 use ratatui::prelude::CrosstermBackend;
 use ratatui::widgets::Paragraph;
 use ratatui::Terminal as RataTerminal;
-use twilight_gateway::Shard;
+use twilight_gateway::{Event as TwilightGatewayEvent, Shard};
 use twilight_http::Client as HttpClient;
 use twilight_model::gateway::{Intents, ShardId};
 
@@ -30,8 +31,12 @@ mod discord;
 mod select;
 mod ui;
 
+/// The maximum amount for when the client should request guild names.
+const INIT_GUILD_REQ_THRESHOLD: usize = 10;
+
 // TODO: make this infallible
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let token: Cow<str> = match (env::var("TOKEN"), env!("INCLUDED_TOKEN")) {
         (Ok(token), "") if token.as_str() == "" => panic!("empty token"),
         (Ok(token), _) => token.into(),
@@ -39,8 +44,6 @@ fn main() -> anyhow::Result<()> {
         (Err(VarError::NotPresent), token) => token.into(),
         (Err(VarError::NotUnicode(_)), _) => panic!("the token in `TOKEN` is not utf-8")
     };
-
-    let ctx = AppContext {};
 
     let input_devices = audio::list_input_devices()?;
     if input_devices.is_empty() {
@@ -57,25 +60,75 @@ fn main() -> anyhow::Result<()> {
         // TODO: this is static, maybe in the future this needs to be dynamic
         CpalMediaSource::from_device::<f32>(&input_device, &stream_config)?;
 
+    let mut shard = Shard::new(ShardId::ONE, token.to_string(), Intents::GUILD_VOICE_STATES);
+
+    let http = HttpClient::new(token.to_string());
+
+    let ready = discord::wait_for_ready(&mut shard).await?;
+    let guild_count = ready.guilds.len();
+
+    let init_guilds = match guild_count {
+        0 => None,
+        l if l > INIT_GUILD_REQ_THRESHOLD => None,
+        _ => {
+            let mut guilds = Vec::with_capacity(guild_count);
+            for guild in ready.guilds {
+                let guild = http.guild(guild.id).await?.model().await?;
+                guilds.push(guild);
+            }
+            Some(guilds)
+        }
+    };
+
+    let ctx = Arc::new(AppContext { http });
+
+    let selected_to_join = match init_guilds {
+        None => None,
+        Some(init_guilds) => select::select_channel_to_join(init_guilds, &ctx).await?
+    };
+
+    if let Some((guild, channel)) = selected_to_join {
+        dbg!(&guild.name);
+        dbg!(&channel.name);
+    }
+
+    std::process::exit(0);
+
+    loop {
+        let event = match shard.next_event().await {
+            Ok(event) => event,
+            Err(source) => {
+                if source.is_fatal() {
+                    break;
+                }
+
+                continue;
+            }
+        };
+
+        tokio::spawn(handle_event(event, ctx.clone()));
+    }
+
     Ok(())
+}
 
-    // let mut terminal = setup_terminal()?;
-    // let input_devices = audio::list_input_devices()?;
-    // ui::run_select(&mut terminal, &ctx, input_devices.as_slice())?;
-    // restore_terminal(&mut terminal)?;
-    // Ok(())
+async fn handle_event(event: TwilightGatewayEvent, ctx: Arc<AppContext>) {
+    use TwilightGatewayEvent as E;
+    match event {
+        E::Ready(event) => {
+            let event = event.deref();
+            println!("ready.guilds: {}", event.guilds.len());
+        }
+        _ => ()
+    }
 
-    // let mut shard = Shard::new(
-    //     ShardId::ONE,
-    //     token.to_string(),
-    //     Intents::GUILD_VOICE_STATES
-    // );
-    //
-    // let http = Arc::new(HttpClient::new(token.to_string()));
+    // dbg!(event);
 }
 
 // is public to allow 'ui::run_select' to be public
-pub struct AppContext {}
+pub struct AppContext {
+    pub http: HttpClient
+}
 
 fn setup_terminal() -> anyhow::Result<Terminal> {
     let mut stdout = io::stdout();
