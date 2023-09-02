dd3556e12e11f3378752ba28e244281665b060bc
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-09-02 13:10:41 +0200

    fmt

--- src/select.rs
+++ src/select.rs
@@ -47,23 +47,46 @@ pub fn select_input_device(
 pub fn select_stream_config(selected_input_device: &Device) -> anyhow::Result<StreamConfig> {
     let theme = THEME.deref();
 
-    let input_configs: Vec<_> = selected_input_device.supported_input_configs()?.filter(|range| range.channels() <= 2).collect();
+    let input_configs: Vec<_> = selected_input_device
+        .supported_input_configs()?
+        .filter(|range| range.channels() <= 2)
+        .collect();
     let selected_config = match input_configs.len() {
-        0 => return Err(anyhow::Error::msg("no supported input config found that is compatible with AudioWarp")),
+        0 => {
+            return Err(anyhow::Error::msg(
+                "no supported input config found that is compatible with AudioWarp"
+            ))
+        }
         1 => input_configs.into_iter().next().expect("not empty"),
         _ => {
-            let input_config_channels: Vec<_> = input_configs.iter().map(|range| match range.channels() {
-                0 => unimplemented!(),
-                1 => Cow::Borrowed("mono"),
-                2 => Cow::Borrowed("stereo"),
-                c => Cow::Owned(c.to_string())
-            }).collect();
+            let input_config_channels: Vec<_> = input_configs
+                .iter()
+                .map(|range| match range.channels() {
+                    0 => unimplemented!(),
+                    1 => "mono",
+                    2 => "stereo",
+                    _ => unreachable!("filtered previously")
+                })
+                .collect();
             let mut select = Select::with_theme(theme);
-            select.with_prompt("Select Channel Count").items(input_config_channels.as_slice());
-            input_configs.iter().enumerate().find(|(_, range)| range.channels() == 2).map(|(i, _)| i).iter().for_each(|i| { select.default(*i);});
+            select
+                .with_prompt("Select Channel Count")
+                .items(input_config_channels.as_slice());
+            input_configs
+                .iter()
+                .enumerate()
+                .find(|(_, range)| range.channels() == 2)
+                .map(|(i, _)| i)
+                .iter()
+                .for_each(|i| {
+                    select.default(*i);
+                });
             let index = select.interact()?;
 
-            input_configs.into_iter().nth(index).expect("index is from this vec")
+            input_configs
+                .into_iter()
+                .nth(index)
+                .expect("index is from this vec")
         }
     };
 
@@ -72,8 +95,15 @@ pub fn select_stream_config(selected_input_device: &Device) -> anyhow::Result<St
     let sample_rate = match max_sample_rate == min_sample_rate {
         true => max_sample_rate,
         false => {
-            let rates: Vec<_> = [("max", max_sample_rate), ("min", min_sample_rate)].iter().map(|(name, rate)| format!("{} {}", name, SampleRate::from(*rate))).collect();
-            let index = Select::with_theme(theme).with_prompt("Select Sample Rate").items(rates.as_slice()).default(0).interact()?;
+            let rates: Vec<_> = [("max", max_sample_rate), ("min", min_sample_rate)]
+                .iter()
+                .map(|(name, rate)| format!("{} {}", name, SampleRate::from(*rate)))
+                .collect();
+            let index = Select::with_theme(theme)
+                .with_prompt("Select Sample Rate")
+                .items(rates.as_slice())
+                .default(0)
+                .interact()?;
             match index {
                 0 => max_sample_rate,
                 1 => min_sample_rate,
