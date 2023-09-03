417db3a8cec76bf7bdfbb03932afed642be469a1
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-09-03 11:30:35 +0200

    on startup select now works

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use std::borrow::Cow;
 use std::env::VarError;
 use std::io::Stdout;
 use std::ops::Deref;
-use std::sync::Arc;
-use std::time::Duration;
-use std::{env, io};
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::{env, io, thread};
+use std::collections::HashMap;
+use std::future::IntoFuture;
+use songbird::join::Join;
 
-use cpal::traits::{DeviceTrait, HostTrait};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::SampleFormat;
 use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{
@@ -18,9 +21,15 @@ use crossterm::{event, execute};
 use ratatui::prelude::CrosstermBackend;
 use ratatui::widgets::Paragraph;
 use ratatui::Terminal as RataTerminal;
-use twilight_gateway::{Event as TwilightGatewayEvent, Shard};
+use songbird::shards::TwilightMap;
+use songbird::{Call, Songbird};
+use songbird::driver::Bitrate;
+use tokio::runtime::Runtime;
+use twilight_gateway::{ConfigBuilder, Event as TwilightGatewayEvent, Shard};
 use twilight_http::Client as HttpClient;
 use twilight_model::gateway::{Intents, ShardId};
+use twilight_model::gateway::payload::outgoing::update_presence::UpdatePresencePayload;
+use twilight_model::gateway::presence::{Activity, ActivityEmoji, ActivityType, Status};
 
 use crate::audio::CpalMediaSource;
 
@@ -37,6 +46,7 @@ const INIT_GUILD_REQ_THRESHOLD: usize = 10;
 // TODO: make this infallible
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+
     let token: Cow<str> = match (env::var("TOKEN"), env!("INCLUDED_TOKEN")) {
         (Ok(token), "") if token.as_str() == "" => panic!("empty token"),
         (Ok(token), _) => token.into(),
@@ -51,18 +61,43 @@ async fn main() -> anyhow::Result<()> {
     }
     let (input_device, input_device_name) = select::select_input_device(input_devices)?;
 
-    for config in input_device.supported_input_configs()? {
-        dbg!(config);
-    }
-
     let stream_config = select::select_stream_config(&input_device)?;
     let (media_source, stream) =
         // TODO: this is static, maybe in the future this needs to be dynamic
         CpalMediaSource::from_device::<f32>(&input_device, &stream_config)?;
 
-    let mut shard = Shard::new(ShardId::ONE, token.to_string(), Intents::GUILD_VOICE_STATES);
+    let activity: Activity = Activity {
+        application_id: None,
+        assets: None,
+        buttons: Vec::new(),
+        created_at: None,
+        details: None,
+        emoji: None,
+        flags: None,
+        id: None,
+        instance: None,
+        kind: ActivityType::Custom,
+        name: String::from("AudioWarp"),
+        party: None,
+        secrets: None,
+        state: String::from("✨ Warping 𝐀𝐮𝐝𝐢𝐨").into(),
+        timestamps: None,
+        url: None,
+    };
+
+    let mut shard = Shard::with_config(
+        ShardId::ONE,
+        ConfigBuilder::new(token.to_string(), Intents::GUILD_VOICE_STATES).presence(UpdatePresencePayload {
+            activities: vec![activity],
+            afk: false,
+            since: None,
+            status: Status::Online,
+        }).build()
+    );
+
 
     let http = HttpClient::new(token.to_string());
+    let user_id = http.current_user().await?.model().await?.id;
 
     let ready = discord::wait_for_ready(&mut shard).await?;
     let guild_count = ready.guilds.len();
@@ -80,7 +115,10 @@ async fn main() -> anyhow::Result<()> {
         }
     };
 
-    let ctx = Arc::new(AppContext { http });
+    let twilight_map = TwilightMap::new(HashMap::from([(shard.id().number(), shard.sender())]));
+    let songbird = Songbird::twilight(Arc::new(twilight_map), user_id);
+
+    let ctx = Arc::new(AppContext { http, songbird });
 
     let selected_to_join = match init_guilds {
         None => None,
@@ -88,23 +126,28 @@ async fn main() -> anyhow::Result<()> {
     };
 
     if let Some((guild, channel)) = selected_to_join {
-        dbg!(&guild.name);
-        dbg!(&channel.name);
+        let ctx = ctx.clone();
+        tokio::spawn(async move {
+            // usually we check if we have only one call, but here it has to be empty
+            let call = ctx.songbird.join(guild.id, channel.id).await.unwrap();
+            let mut call = call.lock().await;
+            call.deafen(true).await.unwrap();
+            call.set_bitrate(Bitrate::Max);
+            let input = media_source.into_input();
+            call.play_only_input(input);
+        });
     }
 
-    std::process::exit(0);
+    stream.play()?;
 
     loop {
         let event = match shard.next_event().await {
             Ok(event) => event,
-            Err(source) => {
-                if source.is_fatal() {
-                    break;
-                }
-
-                continue;
-            }
+            Err(source) if source.is_fatal() => break,
+            Err(_) => continue,
         };
+
+        ctx.songbird.process(&event).await;
 
         tokio::spawn(handle_event(event, ctx.clone()));
     }
@@ -115,19 +158,27 @@ async fn main() -> anyhow::Result<()> {
 async fn handle_event(event: TwilightGatewayEvent, ctx: Arc<AppContext>) {
     use TwilightGatewayEvent as E;
     match event {
-        E::Ready(event) => {
-            let event = event.deref();
-            println!("ready.guilds: {}", event.guilds.len());
+        E::GuildDelete(_) => {
+            // TODO: disconnect from that voice, if connected
         }
+        E::GuildUpdate(_) => {
+            // TODO: update ui indicator
+        }
+        E::VoiceStateUpdate(_) => {
+            // TODO: update ui indicator and songbird connection
+        }
+        E::InteractionCreate(_) => {
+            // TODO: listen to commands
+        }
+
         _ => ()
     }
-
-    // dbg!(event);
 }
 
 // is public to allow 'ui::run_select' to be public
 pub struct AppContext {
-    pub http: HttpClient
+    pub http: HttpClient,
+    pub songbird: Songbird
 }
 
 fn setup_terminal() -> anyhow::Result<Terminal> {
