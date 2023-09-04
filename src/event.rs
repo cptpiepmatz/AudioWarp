610915098fa0892646ae28489f1cb7f85d83878e
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-09-04 18:23:26 +0200

    removed todo macros in event to avoid crashes

--- src/event.rs
+++ src/event.rs
@@ -32,20 +32,16 @@ pub async fn handle_event(event: TwilightGatewayEvent, ctx: impl AsRef<AppContex
 
 async fn handle_guild_delete(event: GuildDelete, ctx: &AppContext) {
     // TODO: disconnect from that voice, if connected
-    todo!()
 }
 
 async fn handle_guild_update(event: GuildUpdate, ctx: &AppContext) {
     // TODO: update ui indicator
-    todo!()
 }
 
 async fn handle_voice_state_update(event: VoiceStateUpdate, ctx: &AppContext) {
     // TODO: update ui indicator and songbird connection
-    todo!()
 }
 
 async fn handle_interaction_create(event: InteractionCreate, ctx: &AppContext) {
     // TODO: listen to commands
-    todo!()
 }
