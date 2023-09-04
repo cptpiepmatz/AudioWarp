d093a95b34bc847a44c4cac306c19ef30e328cc8
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-09-04 13:09:49 +0200

    abstracted the Arc away in the handle_event function

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -1,5 +1,3 @@
-use std::sync::Arc;
-
 use twilight_gateway::error::ReceiveMessageError;
 use twilight_gateway::Shard;
 use twilight_model::gateway::event::{Event, Event as TwilightGatewayEvent};
@@ -19,33 +17,35 @@ pub async fn wait_for_ready(shard: &mut Shard) -> Result<Ready, ReceiveMessageEr
     }
 }
 
-pub async fn handle_event(event: TwilightGatewayEvent, ctx: Arc<AppContext>) {
+pub async fn handle_event(event: TwilightGatewayEvent, ctx: impl AsRef<AppContext>) {
+    let ctx = ctx.as_ref();
+
     use twilight_model::gateway::event::Event as E;
     match event {
-        E::GuildDelete(event) => handle_guild_delete(event).await,
-        E::GuildUpdate(event) => handle_guild_update(*event).await,
-        E::VoiceStateUpdate(event) => handle_voice_state_update(*event).await,
-        E::InteractionCreate(event) => handle_interaction_create(*event).await,
+        E::GuildDelete(event) => handle_guild_delete(event, ctx).await,
+        E::GuildUpdate(event) => handle_guild_update(*event, ctx).await,
+        E::VoiceStateUpdate(event) => handle_voice_state_update(*event, ctx).await,
+        E::InteractionCreate(event) => handle_interaction_create(*event, ctx).await,
         _ => ()
     }
 }
 
-async fn handle_guild_delete(event: GuildDelete) {
+async fn handle_guild_delete(event: GuildDelete, ctx: &AppContext) {
     // TODO: disconnect from that voice, if connected
     todo!()
 }
 
-async fn handle_guild_update(event: GuildUpdate) {
+async fn handle_guild_update(event: GuildUpdate, ctx: &AppContext) {
     // TODO: update ui indicator
     todo!()
 }
 
-async fn handle_voice_state_update(event: VoiceStateUpdate) {
+async fn handle_voice_state_update(event: VoiceStateUpdate, ctx: &AppContext) {
     // TODO: update ui indicator and songbird connection
     todo!()
 }
 
-async fn handle_interaction_create(event: InteractionCreate) {
+async fn handle_interaction_create(event: InteractionCreate, ctx: &AppContext) {
     // TODO: listen to commands
     todo!()
 }
