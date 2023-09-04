d611eb93e7e4c646ef912f16dcd47f60f559c668
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-09-04 18:23:06 +0200

    added a trait on Songbird to clear calls

--- src/util.rs
+++ src/util.rs
@@ -1,1 +1,27 @@
+use async_trait::async_trait;
+use songbird::error::JoinResult;
+use songbird::id::GuildId;
+use songbird::Songbird;
 
+#[async_trait]
+pub trait SongbirdUtil {
+    async fn clear_calls(&self, guild_id: impl Into<GuildId> + Send) -> JoinResult<()>;
+}
+
+#[async_trait]
+impl SongbirdUtil for Songbird {
+    async fn clear_calls(&self, guild_id: impl Into<GuildId> + Send) -> JoinResult<()> {
+        let guild_id = guild_id.into();
+        let other_guild_ids: Vec<_> = self
+            .iter()
+            .filter(|(g_id, _)| *g_id != guild_id)
+            .map(|(g_id, _)| g_id)
+            .collect();
+
+        for other_guild_id in other_guild_ids {
+            self.remove(other_guild_id).await?;
+        }
+
+        Ok(())
+    }
+}
